246129a8c22f9a7f280c8a19618627505e6c3176
Mohammed AbuIssa <sulpasspartout@alexu.edu.eg>
2015-08-13 18:24:18 +0200

    scale test

--- KarakibLab/TranslateRotate.xaml.cs
+++ KarakibLab/TranslateRotate.xaml.cs
@@ -32,6 +32,7 @@ namespace KarakibLab
             
         }
         PathFigure square = new PathFigure();
+        Point currentpointer;
         /// <summary>
         /// Invoked when this page is about to be displayed in a Frame.
         /// </summary>
@@ -42,7 +43,7 @@ namespace KarakibLab
             path p = new path();
             PathGeometry geo = new PathGeometry();
             geo.Figures.Add(square);
-            square.Segments.Add(new LineSegment { Point = new Point(0, 0) });
+            square.StartPoint = new Point(0, 0);
             square.Segments.Add(new LineSegment { Point = new Point(50, 0) });
             square.Segments.Add(new LineSegment { Point = new Point(50, 50) });
             square.Segments.Add(new LineSegment { Point = new Point(0, 50) });
@@ -62,22 +63,34 @@ namespace KarakibLab
 
         }
         //scale
+        //up
         private void Button_Click_2(object sender, RoutedEventArgs e)
-        {
-
+        {//to be completed with angles geometry 
+            square.StartPoint = new Point(square.StartPoint.X - 5,square.StartPoint.Y -5);
+            (square.Segments[0] as LineSegment).Point = new Point((square.Segments[0] as LineSegment).Point.X + 5, (square.Segments[0] as LineSegment).Point.Y - 5);
+            (square.Segments[1] as LineSegment).Point = new Point((square.Segments[1] as LineSegment).Point.X + 5, (square.Segments[1] as LineSegment).Point.Y + 5);
+            (square.Segments[2] as LineSegment).Point = new Point((square.Segments[2] as LineSegment).Point.X - 5, (square.Segments[2] as LineSegment).Point.Y + 5);
         }
-
+        //down
         private void Button_Click_3(object sender, RoutedEventArgs e)
         {
 
         }
         //move
-        private void g_ManipulationDelta(object sender, ManipulationDeltaRoutedEventArgs e)
+        private void g_PointerMoved(object sender, PointerRoutedEventArgs e)
         {
-            for (int i = 0; i < square.Segments.Count; i++)
-                (square.Segments[i] as LineSegment).Point = new Point((square.Segments[i] as LineSegment).Point.X + e.Delta.Translation.X, (square.Segments[i] as LineSegment).Point.Y + e.Delta.Translation.Y);
+            square.StartPoint = new Point(square.StartPoint.X + (e.GetCurrentPoint(sender as UIElement).Position.X - currentpointer.X), square.StartPoint.Y + (e.GetCurrentPoint(sender as UIElement).Position.Y - currentpointer.Y));   
+            foreach (LineSegment item in square.Segments)
+                item.Point = new Point(item.Point.X + (e.GetCurrentPoint(sender as UIElement).Position.X - currentpointer.X), item.Point.Y + (e.GetCurrentPoint(sender as UIElement).Position.Y - currentpointer.Y));
+            currentpointer = e.GetCurrentPoint(sender as UIElement).Position;
+        }
+
+        private void g_PointerPressed(object sender, PointerRoutedEventArgs e)
+        {
+            currentpointer = e.GetCurrentPoint((UIElement)sender).Position;
         }
         
+
 
     }
 }
