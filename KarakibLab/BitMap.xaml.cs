b09097e7c305fcc28855cab4491bac29679a76d7
Mohammed AbuIssa <sulpasspartout@alexu.edu.eg>
2015-08-24 07:33:41 +0200

    end of day commit

--- KarakibLab/BitMap.xaml.cs
+++ KarakibLab/BitMap.xaml.cs
@@ -31,14 +31,14 @@ namespace KarakibLab
         /// This parameter is typically used to configure the page.</param>
         protected async override void OnNavigatedTo(NavigationEventArgs e)
         {
-            Sticker t = new Circle(new Point(0, 0), 400);
+            Rectangle t = new Rectangle { Height = 400, Width = 240, Fill = new SolidColorBrush(Colors.Black) };
             t.Opacity = 1;
             G.Children.Add(t);
             RenderTargetBitmap render = new RenderTargetBitmap();
             await render.RenderAsync(t);
             var result = await render.GetPixelsAsync();
             const int ScaleFactor = 2;
-            WriteableBitmap source = new WriteableBitmap(ScaleFactor*render.PixelWidth,ScaleFactor*render.PixelHeight);
+            WriteableBitmap source = new WriteableBitmap(ScaleFactor * render.PixelWidth, ScaleFactor * render.PixelHeight);
             using (Stream stream = source.PixelBuffer.AsStream())
             {
                 Byte[] res = new Byte[source.PixelHeight * source.PixelWidth * 4];
@@ -48,8 +48,8 @@ namespace KarakibLab
                 }
                 //عيب يا ابو عيسي :P
                 Byte[] kareem = result.ToArray();
-                int Hoffset = (int)(0.5 * render.PixelWidth);
-                int Voffset = (int)(0.5 * render.PixelHeight);
+                int Hoffset = (int)((G.ActualWidth - render.PixelWidth)/2);
+                int Voffset = (int)((G.ActualHeight - render.PixelHeight)/2);
                 for (int i = 0,j=0; i+4 < source.PixelWidth*render.PixelHeight*4+Voffset * source.PixelWidth * 4; i =((j/(render.PixelWidth*4))+Voffset)*source.PixelWidth*4+i%(render.PixelWidth*4))
                 {
                     if (kareem[j + 1] == 0 && kareem[j] == 0 && kareem[j + 2] == 0 && kareem[j + 3] == 255)
---
+++ KarakibLab/Template.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI;
+using Windows.UI.Xaml.Shapes;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Media.Imaging;
+
+namespace KarakibLab
+{
+    using path = Windows.UI.Xaml.Shapes.Path;
+    class Template : path
+    {
+        public Template(String Data)
+        {
+            this.Data = this.Parse(Data);
+        }
+        private PathGeometry Parse(String Data)
+        {
+
+        }
+        private String[,] Spliter(String Data)
+        {
+
+        }
+    }
+}
